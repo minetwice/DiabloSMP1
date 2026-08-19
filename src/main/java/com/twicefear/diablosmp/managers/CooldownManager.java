4749ab5c615b029498e62810625c2dfec51a7f3a
Twicefear <ytd82774@gmail.com>
2026-08-19 19:22:48 +0530

    Improve CooldownManager - unicode ready ActionBar + continuous display task

--- src/main/java/com/twicefear/diablosmp/managers/CooldownManager.java
+++ src/main/java/com/twicefear/diablosmp/managers/CooldownManager.java
@@ -3,8 +3,9 @@ package com.twicefear.diablosmp.managers;
 import com.twicefear.diablosmp.DiabloSMP;
 import com.twicefear.diablosmp.stones.StoneType;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,8 +17,33 @@ public class CooldownManager {
     // UUID -> (StoneType -> (isPrimary -> endTime))
     private final Map<UUID, Map<StoneType, Map<Boolean, Long>>> cooldowns = new HashMap<>();
 
+    // Unicode from resource pack (when textures added)
+    // \uE001 = empty bar, \uE002 = full bar, \uE010 = stone icon, \uE011 = question mark
+    private static final String EMPTY_BAR = "\uE001";
+    private static final String FULL_BAR = "\uE002";
+    private static final String STONE_ICON = "\uE010";
+    private static final String QUESTION = "\uE011";
+
     public CooldownManager(DiabloSMP plugin) {
         this.plugin = plugin;
+        startActionBarTask();
+    }
+
+    private void startActionBarTask() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (plugin.getAbsorbManager().hasAbsorbed(player)) {
+                        StoneType type = plugin.getAbsorbManager().getAbsorbed(player);
+                        sendCooldownActionBar(player, type);
+                    } else {
+                        // Show question mark when no stone absorbed
+                        player.sendActionBar(Component.text("§7" + QUESTION + " §8No Stone Absorbed"));
+                    }
+                }
+            }
+        }.runTaskTimer(plugin, 20L, 10L); // every 0.5 sec
     }
 
     public void startCooldown(Player player, StoneType type, boolean primary) {
@@ -50,16 +76,24 @@ public class CooldownManager {
         long primaryRem = getRemaining(player, type, true);
         long secondaryRem = getRemaining(player, type, false);
 
-        // This will be replaced by resource pack unicode bars later
-        String primaryBar = primaryRem > 0 ? "§c" + (primaryRem / 1000) + "s" : "§aReady";
-        String secondaryBar = secondaryRem > 0 ? "§c" + (secondaryRem / 1000) + "s" : "§aReady";
+        // Build bars (will look perfect once resource pack fonts are added)
+        String primaryPart;
+        String secondaryPart;
 
-        Component msg = Component.text("§e[Primary] ")
-                .append(Component.text(primaryBar))
-                .append(Component.text("  §8|  "))
-                .append(Component.text("§e[Secondary] "))
-                .append(Component.text(secondaryBar));
+        if (primaryRem > 0) {
+            primaryPart = "§c" + EMPTY_BAR + " " + (primaryRem / 1000) + "s";
+        } else {
+            primaryPart = "§a" + FULL_BAR + " Ready";
+        }
 
+        if (secondaryRem > 0) {
+            secondaryPart = "§c" + EMPTY_BAR + " " + (secondaryRem / 1000) + "s";
+        } else {
+            secondaryPart = "§a" + FULL_BAR + " Ready";
+        }
+
+        // Format: [Primary Bar]  [Stone Icon]  [Secondary Bar]
+        Component msg = Component.text(primaryPart + "  §f" + STONE_ICON + "  " + secondaryPart);
         player.sendActionBar(msg);
     }
 
