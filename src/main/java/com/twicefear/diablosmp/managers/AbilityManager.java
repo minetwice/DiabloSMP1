201724851021b4eb37d7948ab9123d1b9bda1167
Twicefear <ytd82774@gmail.com>
2026-08-19 19:08:22 +0530

    Update AbilityManager with Tempest, Shadow, Frostbite abilities

--- src/main/java/com/twicefear/diablosmp/managers/AbilityManager.java
+++ src/main/java/com/twicefear/diablosmp/managers/AbilityManager.java
@@ -1,8 +1,7 @@
 package com.twicefear.diablosmp.managers;
 
 import com.twicefear.diablosmp.DiabloSMP;
-import com.twicefear.diablosmp.abilities.EarthquakeAbility;
-import com.twicefear.diablosmp.abilities.InfernalAbility;
+import com.twicefear.diablosmp.abilities.*;
 import com.twicefear.diablosmp.stones.StoneType;
 import org.bukkit.entity.Player;
 
@@ -11,11 +10,17 @@ public class AbilityManager {
     private final DiabloSMP plugin;
     private final EarthquakeAbility earthquakeAbility;
     private final InfernalAbility infernalAbility;
+    private final TempestAbility tempestAbility;
+    private final ShadowAbility shadowAbility;
+    private final FrostbiteAbility frostbiteAbility;
 
     public AbilityManager(DiabloSMP plugin) {
         this.plugin = plugin;
         this.earthquakeAbility = new EarthquakeAbility(plugin);
         this.infernalAbility = new InfernalAbility(plugin);
+        this.tempestAbility = new TempestAbility(plugin);
+        this.shadowAbility = new ShadowAbility(plugin);
+        this.frostbiteAbility = new FrostbiteAbility(plugin);
     }
 
     public void execute(Player player, StoneType type, boolean primary) {
@@ -28,7 +33,19 @@ public class AbilityManager {
                 if (primary) infernalAbility.primary(player);
                 else infernalAbility.secondary(player);
             }
-            // More abilities will be added here
+            case TEMPEST_ORB -> {
+                if (primary) tempestAbility.primary(player);
+                else tempestAbility.secondary(player);
+            }
+            case SHADOW_FANG -> {
+                if (primary) shadowAbility.primary(player);
+                else shadowAbility.secondary(player);
+            }
+            case FROSTBITE_CRYSTAL -> {
+                if (primary) frostbiteAbility.primary(player);
+                else frostbiteAbility.secondary(player);
+            }
+            // Remaining stones still under development
             default -> {
                 player.sendMessage("§e" + type.getDisplayName() + " §7ability is under development (Primary: " + primary + ")");
                 player.getWorld().spawnParticle(type.getParticle(), player.getLocation().add(0, 1, 0), 60, 1.2, 1.2, 1.2, 0.08);
