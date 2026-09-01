cd636a775b5551fd3a4bb2da1341e208be0f07bc
Twicefear <ytd82774@gmail.com>
2026-09-01 21:21:06 +0530

    Fix Particle enum names for Paper 1.21.1 (SPELL_INSTANT->INSTANT_EFFECT, VILLAGER_HAPPY->HAPPY_VILLAGER, TOWN_AURA->MYCELIUM)

--- src/main/java/com/sdmine/elementalcore/variants/ParticleEffectHandler.java
+++ src/main/java/com/sdmine/elementalcore/variants/ParticleEffectHandler.java
@@ -18,12 +18,12 @@ public class ParticleEffectHandler {
             case "WATER_DRIP_LAVA_SPARK" -> { loc.getWorld().spawnParticle(Particle.DRIPPING_WATER, loc, 2, 0.3, 0.5, 0.3, 0); loc.getWorld().spawnParticle(Particle.LAVA, loc, 1, 0.3, 0.5, 0.3, 0); }
             case "WATER_SPLASH_BUBBLE" -> { loc.getWorld().spawnParticle(Particle.SPLASH, loc, 3, 0.3, 0.5, 0.3, 0.1); loc.getWorld().spawnParticle(Particle.BUBBLE_POP, loc.clone().add(0,-0.5,0), 2, 0.3, 0.3, 0.3, 0); }
             case "FLAME_SWIRL_CLOUD" -> { loc.getWorld().spawnParticle(Particle.FLAME, loc, 2, 0.3, 0.5, 0.3, 0.02); loc.getWorld().spawnParticle(Particle.CLOUD, loc, 2, 0.3, 0.5, 0.3, 0.01); }
-            case "SNOWFLAKE_CYAN" -> { loc.getWorld().spawnParticle(Particle.SNOWFLAKE, loc, 3, 0.3, 0.5, 0.3, 0.01); loc.getWorld().spawnParticle(Particle.SPELL_INSTANT, loc, 2, 0.3, 0.5, 0.3, 0); }
-            case "GREEN_SPARK_CRACK" -> { loc.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, loc, 3, 0.3, 0.5, 0.3, 0); loc.getWorld().spawnParticle(Particle.BLOCK, loc, 2, 0.3, 0.5, 0.3, 0, org.bukkit.Material.DIRT.createBlockData()); }
+            case "SNOWFLAKE_CYAN" -> { loc.getWorld().spawnParticle(Particle.SNOWFLAKE, loc, 3, 0.3, 0.5, 0.3, 0.01); loc.getWorld().spawnParticle(Particle.INSTANT_EFFECT, loc, 2, 0.3, 0.5, 0.3, 0); }
+            case "GREEN_SPARK_CRACK" -> { loc.getWorld().spawnParticle(Particle.HAPPY_VILLAGER, loc, 3, 0.3, 0.5, 0.3, 0); loc.getWorld().spawnParticle(Particle.BLOCK, loc, 2, 0.3, 0.5, 0.3, 0, org.bukkit.Material.DIRT.createBlockData()); }
             case "LAVA_POP_CRIT" -> { loc.getWorld().spawnParticle(Particle.LAVA, loc, 2, 0.3, 0.5, 0.3, 0); loc.getWorld().spawnParticle(Particle.CRIT, loc, 2, 0.3, 0.5, 0.3, 0); }
             case "DRAGON_BREATH_PORTAL" -> { loc.getWorld().spawnParticle(Particle.DRAGON_BREATH, loc, 2, 0.3, 0.5, 0.3, 0.01); loc.getWorld().spawnParticle(Particle.PORTAL, loc, 3, 0.3, 0.5, 0.3, 0.5); }
             case "SWEEP_CLOUD" -> { loc.getWorld().spawnParticle(Particle.SWEEP_ATTACK, loc, 1, 0.3, 0.5, 0.3, 0); loc.getWorld().spawnParticle(Particle.CLOUD, loc, 2, 0.3, 0.5, 0.3, 0.01); }
-            case "END_ROD_GLOWSTONE" -> { loc.getWorld().spawnParticle(Particle.END_ROD, loc, 3, 0.3, 0.5, 0.3, 0.02); loc.getWorld().spawnParticle(Particle.TOWN_AURA, loc, 2, 0.3, 0.5, 0.3, 0); }
+            case "END_ROD_GLOWSTONE" -> { loc.getWorld().spawnParticle(Particle.END_ROD, loc, 3, 0.3, 0.5, 0.3, 0.02); loc.getWorld().spawnParticle(Particle.MYCELIUM, loc, 2, 0.3, 0.5, 0.3, 0); }
             case "TRI_COLOR_BURST" -> { DustOptions r = new DustOptions(Color.RED, 1.2f), b = new DustOptions(Color.BLUE, 1.2f), y = new DustOptions(Color.YELLOW, 1.2f); loc.getWorld().spawnParticle(Particle.DUST, loc.clone().add(0.2,0,0), 1, 0,0,0,0, r); loc.getWorld().spawnParticle(Particle.DUST, loc.clone().add(-0.2,0,0), 1, 0,0,0,0, b); loc.getWorld().spawnParticle(Particle.DUST, loc.clone().add(0,0.2,0), 1, 0,0,0,0, y); }
             default -> {}
         }
